2cde6878e8796cc1b80e408823de7253b3651773
yihuwangji <aladaya@gmail.com>
2026-05-09 16:38:31 +0200

    Use public auth redirect URL

--- online-config.example.js
+++ online-config.example.js
@@ -1,5 +1,6 @@
 window.ONLINE_CONFIG = {
   SUPABASE_URL: "https://YOUR_PROJECT_REF.supabase.co",
   SUPABASE_ANON_KEY: "YOUR_SUPABASE_ANON_OR_PUBLISHABLE_KEY",
-  STORAGE_BUCKET: "invoice-files"
+  STORAGE_BUCKET: "invoice-files",
+  APP_URL: "https://your-domain.example/facturas/online.html"
 };
--- online-config.js
+++ online-config.js
@@ -1,5 +1,6 @@
 window.ONLINE_CONFIG = {
   SUPABASE_URL: "https://anyjfioxwfxzbyavqxms.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFueWpmaW94d2Z4emJ5YXZxeG1zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNjM0MTgsImV4cCI6MjA5MzczOTQxOH0.KpxQrsEHt4t4rW6p_TU2FkBbR2O-y0q08csOfuaUCXA",
-  STORAGE_BUCKET: "invoice-files"
+  STORAGE_BUCKET: "invoice-files",
+  APP_URL: "https://yihuwangji.github.io/facturas/online.html"
 };
